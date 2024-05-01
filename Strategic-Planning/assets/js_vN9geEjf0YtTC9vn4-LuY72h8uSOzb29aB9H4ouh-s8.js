271627e3e06f2d796a54a5a4618f6725a92d8ef0
peterdudesign <140740465+peterdudesign@users.noreply.github.com>
2024-05-01 13:47:13 -0400

    Strategic Planning
    Adding Strategic Planning

---
+++ Strategic-Planning/assets/js_vN9geEjf0YtTC9vn4-LuY72h8uSOzb29aB9H4ouh-s8.js
@@ -0,0 +1,2 @@
+/* @license GNU-GPL-2.0-or-later https://www.drupal.org/licensing/faq */
+!function(a){"use strict";a.behaviors.fancybox_init={attach:function(a,n){document.querySelector("[data-fancybox]")&&Fancybox.bind("[data-fancybox]",{});}};}(Drupal);;
